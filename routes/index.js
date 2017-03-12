fbae3c3791a1d6318a71933a2ee7d1c3340e46a7
labarrerita <labarrerita@uniandes.edu.co>
2017-03-11 19:22:59 -0500

    cambios cosméticos y frustración por no lograr que aparezcan todos los thumbnails

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('SDA', { title: 'Tarea módulo 2 semana 1',
+  res.render('SDA', { title: 'Bienvenidos',
   saludo: 'arte + educación',
       texto1: 'Este espacio se dedica a la promoción y divulgación de proyectos pedagógicos, curatoriales y de investigación en, para y a través del arte, invitando a diversas instituciones educativas, museos, galerías, organizaciones y artistas para hacer aportes que favorezcan la producción de conocimiento y el trabajo colaborativo.',
       texto2: 'Las dinámicas del arte contemporáneo y su estrecha relación con la educación, la cual está en boga actualmente, abren nuevas posibilidades en la construcción de conocimiento y de comunidad a través del trabajo colaborativo e interdisciplinario, a partir de experiencias que llevan a nuevas preguntas y discusiones.',
